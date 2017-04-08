c0a79df9e47a67a269d766b0a0f07d5cc482a9e9
xywei <wxy0516@gmail.com>
2017-04-08 16:37:01 +0800

    boost ppi for pdfview

--- .emacs.d/JxW_config/org/pdfview.el
+++ .emacs.d/JxW_config/org/pdfview.el
@@ -2,3 +2,7 @@
 (delete '("\\.pdf\\'" . default) org-file-apps)
 (add-to-list 'org-file-apps '("\\.pdf\\'" . (lambda (file link) (org-pdfview-open link))))
 (add-to-list 'org-file-apps '("\\.pdf::\\([[:digit:]]+\\)\\'" . (lambda (file link) (org-pdfview-open link))))
+
+;; Set a higher dpi
+(require 'doc-view)
+(setq doc-view-resolution 400)
